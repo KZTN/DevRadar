91ddbf7033cc448bc3d755c21a8bd2135443c97f
kztn <kakaiocesar2@gmail.com>
2020-04-02 23:43:51 -0300

    ✨ Initial commit

---
+++ backend/src/app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const routes = require('./routes');
+class App {
+    constructor() {
+        this.server = express();
+        this.middlewares();
+    }
+    middlewares() {
+        server.use(express.json());
+        server.use(routes);
+    }
+}
+module.exports = new App().server;
