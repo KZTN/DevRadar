aadb7d4f7899fbadb8705d7f8278048e2d51bb19
KZTN <kakaiocesar2@gmail.com>
2020-04-03 14:07:09 -0300

    :art: Created connection to mongodb

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,3 +1,8 @@
+import mongoose from 'mongoose';
 import app from './app';
 
+mongoose.connect(process.env.SERVER_TOKEN, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
 app.listen(3333);
