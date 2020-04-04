68c78ba68e5eafa422f73b8c3e92e84aa43b3066
KZTN <kakaiocesar2@gmail.com>
2020-04-04 10:28:50 -0300

    :lipstick: Refactored App page

--- backend/src/controllers/SearchController.js
+++ backend/src/controllers/SearchController.js
@@ -8,18 +8,18 @@ module.exports = {
         const techsArray = parseStringAsArray(techs);
         const devs = await Dev.find({
             techs: {
-              $in: techsArray,
+                $in: techsArray,
             },
             location: {
-              $near: {
-                $geometry: {
-                  type: 'Point',
-                  coordinates: [longitude, latitude],
+                $near: {
+                    $geometry: {
+                        type: 'Point',
+                        coordinates: [longitude, latitude],
+                    },
+                    $maxDistance: 10000,
                 },
-                $maxDistance: 10000,
-              },
             },
-          });
+        });
         return res.json({ devs });
     },
 };
